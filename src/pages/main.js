6b45fd0158baf20c07ece56a78cc5b2e76086d7a
anyl92 <yulim9592@gmail.com>
2023-08-16 10:19:04 +0900

    feat(mainpage): make main

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,8 +1,23 @@
+import { useNavigate } from "react-router-dom";
 const Main = () => {
+  const navigate = useNavigate();
+  const isLogined = Boolean(localStorage.getItem("user_token"));
+
   return (
     <div>
-      Main Page
-      <h2>dhkfkffkfkf</h2>
+      <h2>안녕하세요? TODO를 작성해봅시다.</h2>
+
+      {isLogined ? (
+        <h6>
+          이미 로그인 되어 있으시군요.{" "}
+          <button onClick={() => navigate("/todo")}>작성 하러 가기</button>
+        </h6>
+      ) : (
+        <h6>
+          <button onClick={() => navigate("/signup")}>회원가입</button>
+          <button onClick={() => navigate("/signin")}>로그인</button>
+        </h6>
+      )}
     </div>
   );
 };
